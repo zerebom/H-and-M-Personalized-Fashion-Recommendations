53865935a2c65345a7268c3816cdc5b57bc3d393
zerebom <ice.choco.pudding.kokoro@gmail.com>
2022-02-27 14:11:26 +0000

    Add 001_tutorial

--- src/metric.py
+++ src/metric.py
@@ -1,5 +1,6 @@
 import numpy as np
-from average_precision import apk
+import pandas as pd
+
 
 def calculate_apk(list_of_preds, list_of_gts):
     # for fast validation this can be changed to operate on dicts of {'cust_id_int': [art_id_int, ...]}
@@ -20,3 +21,70 @@ def eval_sub(sub_csv, skip_cust_with_no_purchases=True):
         if skip_cust_with_no_purchases and (gt == no_purchases_pattern): continue
         apks.append(apk(gt, pred, k=12))
     return np.mean(apks)
+
+
+
+
+def apk(actual, predicted, k=10):
+    """
+    Computes the average precision at k.
+
+    This function computes the average prescision at k between two lists of
+    items.
+
+    Parameters
+    ----------
+    actual : list
+             A list of elements that are to be predicted (order doesn't matter)
+    predicted : list
+                A list of predicted elements (order does matter)
+    k : int, optional
+        The maximum number of predicted elements
+
+    Returns
+    -------
+    score : double
+            The average precision at k over the input lists
+
+    """
+    if len(predicted)>k:
+        predicted = predicted[:k]
+
+    score = 0.0
+    num_hits = 0.0
+
+    for i,p in enumerate(predicted):
+        if p in actual and p not in predicted[:i]:
+            num_hits += 1.0
+            score += num_hits / (i+1.0)
+
+    if not actual:
+        return 0.0
+
+    return score / min(len(actual), k)
+
+def mapk(actual, predicted, k=10):
+    """
+    Computes the mean average precision at k.
+
+    This function computes the mean average prescision at k between two lists
+    of lists of items.
+
+    Parameters
+    ----------
+    actual : list
+             A list of lists of elements that are to be predicted
+             (order doesn't matter in the lists)
+    predicted : list
+                A list of lists of predicted elements
+                (order matters in the lists)
+    k : int, optional
+        The maximum number of predicted elements
+
+    Returns
+    -------
+    score : double
+            The mean average precision at k over the input lists
+
+    """
+    return np.mean([apk(a,p,k) for a,p in zip(actual, predicted)])
