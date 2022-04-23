fc04bce2667c66b0962de5550394605ab4b819d9
zerebom <ice.choco.pudding.kokoro@gmail.com>
2022-04-23 11:59:38 +0000

    Add simple_rankner

--- higu/src/candidacies_dfs.py
+++ higu/src/candidacies_dfs.py
@@ -9,67 +9,23 @@ from utils import customer_hex_id_to_int
 
 to_cdf = cudf.DataFrame.from_pandas
 
-
-def candidates_dict2df(candidates_dict) -> cudf.DataFrame:
-    """
-    {'customer_id_a': [article_id_1, article_id_2],'customer_id_b': [article_id_3, article_id_4]}
-    -> pd.DataFrame([[customer_id_a, article_id_1], [customer_id_a, article_id_2],]...)
-    """
-    tuples = []
-    for cust, articles in tqdm(candidates_dict.items()):
-        for art in articles:
-            tuples.append((cust, art))
-
-    df = pd.DataFrame(tuples)
-    df.columns = ["customer_id", "article_id"]
-    cdf = to_cdf(df)
-    cdf = cdf.drop_duplicates().reset_index(drop=True)
-    return cdf
-
-
-def create_nmslib_index(
-    vector_dict: Dict[Any, np.ndarray],
-    efSearch: int,
-    method: str = "hnsw",
-    space: str = "cosinesimil",
-    post: int = 2,
-    efConstruction: int = 300,  # efConstruction の値を大きくするほど検索精度が向上するが、インデックス作成時間が伸びる
-    M: int = 60,  # M の値をある程度まで大きくすると、再現性が向上して検索時間が短くなるが、インデックス作成時間が伸びる (5 ~ 100 が妥当な範囲)
-) -> Tuple[Any, List[Any]]:
-    print("train NMSLib")
-    nmslib_index = nmslib.init(method=method, space=space)
-    embs = np.stack(list(vector_dict.values()))
-    keys = list(vector_dict.keys())
-    nmslib_index.addDataPointBatch(embs)
-
-    index_time_params = {"M": M, "efConstruction": efConstruction, "post": post}
-    nmslib_index.createIndex(index_time_params, print_progress=True)
-
-    # efSearch を大きい値に設定しないと knnQuery で指定した数(k)より少ない結果が返ってきてしまう
-    # https://github.com/nmslib/nmslib/issues/172#issuecomment-281376808
-    # max(k, efSearch) を設定すると良いらしい
-    query_time_params = {"efSearch": efSearch}
-    nmslib_index.setQueryTimeParams(query_time_params)
-    return nmslib_index, keys
-
-
 class AbstractCGBlock:
     def fit(
-        self, input_cdf: cudf.DataFrame, logger, y_cdf: Optional[cudf.DataFrame] = None
-    ) -> Dict[int, List[int]]:
+        self, trans_cdf, art_cdf, cust_cdf, logger, y_cdf,target_customers
+    ) -> pd.DataFrame:
 
-        out_dic = self.transform(input_cdf)
+        self.out_cdf = self.transform(trans_cdf, art_cdf, cust_cdf, target_customers)
+        self.out_cdf['candidate_block_name'] = self.__class__.__name__
+        self.out_cdf['candidate_block_name'] = self.out_cdf['candidate_block_name'].astype("category")
         self.inspect(y_cdf, logger)
+        out_df = self.out_cdf.to_pandas()
+        return out_df
 
-        return out_dic
-
-    def transform(self, input_cdf: cudf.DataFrame) -> Dict[int, List[int]]:
+    def transform(self, trans_cdf, art_cdf, cust_cdf, target_customers) -> pd.DataFrame:
         """
         以下の処理を書いてください
-        1. input_cdf を加工して、[customer_id,article_id]ペアをレコードにもつself.out_cdf に格納する
-        2. self.out_cdfに self.cdf2dicに渡して[customer_id,List[article_id]]を持つ辞書形式にする
-
-        y_cdf,self.out_cdfがある時、inspectが走ります。
+        1. 引数 を加工して、[customer_id, article_id]ペアをレコードにもつself.out_cdf に格納する
+        y_cdf, self.out_cdfがある時、inspectが走ります。
         """
 
         raise NotImplementedError()
@@ -101,117 +57,6 @@ class AbstractCGBlock:
                 "either y_cdf or out_cdf isn't defined. so skip cg block inspection."
             )
 
-    @staticmethod
-    def cdf2dic(cdf, key_col, item_col):
-        out_dic = (
-            cdf.to_pandas().groupby(key_col)[item_col].apply(list).to_dict()
-        )  # ユーザごとに商品をリストに変換
-        return out_dic
-
-
-class RuleBase(AbstractCGBlock):
-    """
-    ルールベース
-
-    """
-
-    def __init__(
-        self,
-        file_path,
-        target_customers,
-        key_col="customer_id",
-        item_col="article_id",
-    ):
-
-        self.target_customers = target_customers
-        self.file_path = file_path
-        self.key_col = key_col
-        self.item_col = item_col
-
-    def transform(self, _input_cdf):
-        df = cudf.read_csv(self.file_path).to_pandas()
-
-        df["customer_id"] = customer_hex_id_to_int(df["customer_id"])
-        df["prediction"] = df["prediction"].progress_apply(self.split)
-
-        df = df.explode("prediction").dropna().reset_index(drop=True)
-        df["prediction"] = df["prediction"].astype(int)
-        df.columns = [self.key_col, self.item_col]
-        df = df[df["customer_id"].isin(self.target_customers)]
-
-        self.out_cdf = to_cdf(df)
-        out_dic = self.cdf2dic(self.out_cdf, self.key_col, self.item_col)
-
-        return out_dic
-
-    @staticmethod
-    def split(x):
-        if type(x) is str:
-            return x.split(" ")
-        else:
-            return []
-
-
-class ArticlesSimilartoThoseUsersHavePurchased(AbstractCGBlock):
-    """
-    transactionとアイテムのエンべディングを使って、
-    ユーザが過去の購入商品と似ている商品を取得する
-    """
-
-    def __init__(
-        self,
-        article_emb_dict: Dict[Any, np.ndarray],
-        max_neighbors: int,
-        k_neighbors: int,
-        target_articles: List[int],
-        target_customers: List[int],
-    ) -> None:
-        # NMSLib に検索対象となるベクトルを登録する
-        nmslib_index, registered_article_ids = create_nmslib_index(
-            vector_dict=article_emb_dict, efSearch=max_neighbors
-        )
-        self.nmslib_index = nmslib_index
-        self.lib_idx_2_art_id = {
-            i: art_id for i, art_id in enumerate(registered_article_ids)
-        }
-        self.max_neighbors = max_neighbors
-        self.k_neighbors = k_neighbors
-        self.article_emb_dict = article_emb_dict
-        self.target_articles = target_articles
-        self.target_customers = target_customers
-
-    def transform(self, _trans_cdf):
-        trans_df = _trans_cdf.to_pandas()
-        log_dict = trans_df.groupby(["customer_id"])["article_id"].agg(list).to_dict()
-        self.target_customers = list(
-            set(self.target_customers) & set(list(trans_df["customer_id"].unique()))
-        )
-        target_articles_set = set(self.target_articles)
-        candidates_dict = {}
-        for customer_id in tqdm(self.target_customers):
-            customer_bought_article_ids = log_dict[customer_id]
-            candidate_ids = []
-
-            # ユーザの購入商品ごとに近傍アイテムを取得
-            for article_id in customer_bought_article_ids:
-                article_emb = self.article_emb_dict[article_id]
-                scoutee_neighbor_ids, _ = self.nmslib_index.knnQuery(
-                    article_emb, k=self.k_neighbors
-                )
-                candidate_ids.extend(list(scoutee_neighbor_ids))
-
-            # lib_idx_2_art_id でnsmlibのindex順を article_id に変換
-            candidate_ids = [self.lib_idx_2_art_id[i] for i in candidate_ids]
-
-            # drop duplicates & 指定したarticle_ids 以外除去 & max_neighborsでlimit
-            candidate_ids = list(set(candidate_ids) & target_articles_set)[
-                : self.max_neighbors
-            ]
-            candidates_dict[customer_id] = candidate_ids
-            # TODO: candidates_dict→ out_cdfにする処理が書けるとinspectできます。力尽きて書いてない
-
-        return candidates_dict
-
 
 class LastNWeekArticles(AbstractCGBlock):
     """
@@ -224,8 +69,8 @@ class LastNWeekArticles(AbstractCGBlock):
         self.n_weeks = n_weeks
         self.out_keys = [key_col, item_col]
 
-    def transform(self, _input_cdf):
-        input_cdf = _input_cdf.copy()
+    def transform(self, trans_cdf, art_cdf, cust_cdf, target_customers):
+        input_cdf = trans_cdf.copy()
 
         week_df = (
             input_cdf.groupby(self.key_col)["week"]
@@ -242,62 +87,35 @@ class LastNWeekArticles(AbstractCGBlock):
             input_cdf["diff_week"] <= self.n_weeks, self.out_keys
         ]  # 差分がn週間以内のものを取得
 
-        out_dic = self.cdf2dic(self.out_cdf, self.key_col, self.item_col)
-        return out_dic
+        return self.out_cdf
 
+# class PopularItemsoftheLastWeeks(AbstractCGBlock):
+#     """
+#     最終購入週の人気アイテムtopNを返す
+#     TODO: 最終購入数以外の商品も取得できるようにする
 
-class BoughtItemsAtInferencePhase(AbstractCGBlock):
-    """
-    推論期間に購入した商品をCandidateとして返す
-    (リークだが、正例を増やす最も手っ取り早い方法)
+#     """
 
-    """
+#     def __init__(
+#         self, customer_ids, key_col="customer_id", item_col="article_id", n=12
+#     ):
+#         self.customer_ids = customer_ids
+#         self.key_col = key_col
+#         self.item_col = item_col
+#         self.n = n
 
-    def __init__(
-        self,
-        y_cdf,
-        key_col="customer_id",
-        item_col="article_id",
-    ):
+#     def transform(self, _input_cdf):
+#         input_cdf = _input_cdf.copy()
+#         pop_item_dic = {}
+#         self.popular_items = (
+#             input_cdf.loc[input_cdf["week"] == input_cdf["week"].max()][self.item_col]
+#             .value_counts()
+#             .to_pandas()
+#             .index[: self.n]
+#             .values
+#         )
 
-        self.y_cdf = y_cdf
-        self.key_col = key_col
-        self.item_col = item_col
+#         for cust_id in self.customer_ids:
+#             pop_item_dic[cust_id] = list(self.popular_items)
 
-    def transform(self, _input_cdf):
-        self.out_cdf = self.y_cdf.copy()
-        out_dic = self.cdf2dic(self.out_cdf, self.key_col, self.item_col)
-
-        return out_dic
-
-
-class PopularItemsoftheLastWeeks(AbstractCGBlock):
-    """
-    最終購入週の人気アイテムtopNを返す
-    TODO: 最終購入数以外の商品も取得できるようにする
-
-    """
-
-    def __init__(
-        self, customer_ids, key_col="customer_id", item_col="article_id", n=12
-    ):
-        self.customer_ids = customer_ids
-        self.key_col = key_col
-        self.item_col = item_col
-        self.n = n
-
-    def transform(self, _input_cdf):
-        input_cdf = _input_cdf.copy()
-        pop_item_dic = {}
-        self.popular_items = (
-            input_cdf.loc[input_cdf["week"] == input_cdf["week"].max()][self.item_col]
-            .value_counts()
-            .to_pandas()
-            .index[: self.n]
-            .values
-        )
-
-        for cust_id in self.customer_ids:
-            pop_item_dic[cust_id] = list(self.popular_items)
-
-        return pop_item_dic
+#         return pop_item_dic
--- higu/src/notebook/999_multiple_target_week_mock.py
+++ higu/src/notebook/999_multiple_target_week_mock.py
@@ -9,49 +9,56 @@ def make_train_valid_df():
         phase = "validate" if target_week == 104 else "train"
 
         clipped_trans_cdf = trans_cdf.query(f"target_week - {train_duration_weeks} <= week < target_week") #予測日の前週までのトランザクションが使える
-        clipped_trans_cdf["target_week"] = clipped_trans_cdf["week"] + 1
 
         #(要議論) speed upのためにclipped_trans_cdfに存在するart, custだけの情報を使う
         buyable_art_ids = clipped_trans_cdf['article_id'].unique()
-        buyable_art_cdf = clipped_trans_cdf.query(f"art_id in {buyable_art_ids}")
+        buyable_art_cdf = art_cdf.query(f"art_id in {buyable_art_ids}")
 
         bought_cust_ids = clipped_trans_cdf['customer_id'].unique()
-        bought_cust_cdf = clipped_trans_cdf.query(f"customer_id in {bought_cust_ids}")
+        bought_cust_cdf = cust_cdf.query(f"customer_id in {bought_cust_ids}")
 
 
-        base_cols = [
-            "customer_id",
-            "article_id",
-            "candidate_type",
-            "target_week",
-            "target",
-        ]
 
         y_cdf = make_target_df(clipped_trans_cdf)
 
         #いろんなメソッドを使って候補生成
         base_df = candidate_generation(
-            candidate_blocks, clipped_trans_cdf, buyable_art_cdf, bought_cust_cdf, y_cdf, target_customers = buyable_art_ids
+            candidate_blocks, clipped_trans_cdf, buyable_art_cdf, bought_cust_cdf, y_cdf, target_customers = None
         )
 
         base_df = negative_sampling(base_df, clipped_trans_cdf, y_cdf) #適当に間引くはず
 
 
-        art_feat_df, cust_feat_df, pair_feat_df = feature_generation(
-        feature_blocks, clipped_trans_cdf, buyable_art_cdf, bought_cust_cdf, target_customers = buyable_art_ids
+        art_feat_df, cust_feat_df, pair_feat_df = feature_generation_with_transaction(
+        feature_blocks, clipped_trans_cdf, buyable_art_cdf, bought_cust_cdf #base_dfに入っているuser_id,article_idが必要
     )
 
         # 特徴量作成
         base_df = base_df.merge(cust_feat_df, how="left", on="customer_id")
         base_df = base_df.merge(art_feat_df, how="left", on="article_id")
-        base_df = base_df.merge(pair_feat_df, how="left", on=["article_id", "customer_id"]
-        )
+        base_df = base_df.merge(pair_feat_df, how="left", on=["article_id", "customer_id"])
+        base_df["target_week"] = target_week
+        base_df['y'] = base_df.merge(y_cdf, how='left',on=['customer_id', 'article_id'])["y"].fillna(0).astype(int)
+
+        # base_df.columns ==  [
+        #     "customer_id",
+        #     "article_id",
+        #     "candidate_block_name",
+        #     "target_week",
+        #     "target",
+        # ] + [feature_columns]
 
         if phase == "train":
             # 学習データは複数予測週があるのでconcatする
             train_df = pd.concat([train_df, base_df])
         elif phase == "validate":
             valid_df = base_df.copy()
+
+    # transactionによらない特徴量はゆくゆくはここで実装したい (ex. embbeding特徴量)
+    # train_df = feature_generation_with_article(train_df, article_cdf)
+    # valid_df = feature_generation_with_article(valid_df, article_cdf)
+    # train_df = feature_generation_with_customer(train_df, customer_cdf)
+    # valid_df = feature_generation_with_customer(valid_df, customer_cdf)
 
     train_df['query_id'] = train_df.groupby(['customer_id','target_week']).size().values
     valid_df['query_id'] = valid_df.groupby(['customer_id','target_week']).size().values
