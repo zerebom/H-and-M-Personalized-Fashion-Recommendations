e5c3ca4def3326c037b1bcf94f0bc8193d9b3f6e
zerebom <ice.choco.pudding.kokoro@gmail.com>
2022-04-23 07:52:19 +0000

    Update Mock

--- higu/src/notebook/999_multiple_target_week_mock.py
+++ higu/src/notebook/999_multiple_target_week_mock.py
@@ -1,72 +1,65 @@
-target_weeks = [5, 4, 3, 2, 1, 0]  # test_yから何週間離れているか
+target_weeks = [104,103,102,101,100]  # test_yから何週間離れているか
 
 train_df, valid_df, test_df = pd.DataFrame(), pd.DataFrame(), pd.DataFrame()
 
-for target_week in target_weeks:
 
-    if target_week == 0:
-        phase = "test"
-    elif target_week == 1:
-        phase = "validate"
-    else:
-        phase = "train"
+def make_train_valid_df():
+    train_df, valid_df = pd.DataFrame(), pd.DataFrame()
+    for target_week in target_weeks:
+        phase = "validate" if target_week == 104 else "train"
 
-    if phase in ["train", "validate"]:
-      clipped_trans_cdf = trans_cdf.query("target_weeks+1 + {train_duration_weeks} <t-dat<target_week+1") #予測日の前週までのトランザクションが使える
+        clipped_trans_cdf = trans_cdf.query(f"target_week - {train_duration_weeks} <= week < target_week") #予測日の前週までのトランザクションが使える
+        clipped_trans_cdf["target_week"] = clipped_trans_cdf["week"] + 1
 
-      #(要議論) speed upのためにtrans_cdfに存在するart, custだけの情報を使う
-      buyable_art_ids = clipped_trans_cdf['article_id'].unique()
-      buyable_art_cdf = clipped_trans_cdf.query(f"art_id in {buyable_art_ids}")
+        #(要議論) speed upのためにclipped_trans_cdfに存在するart, custだけの情報を使う
+        buyable_art_ids = clipped_trans_cdf['article_id'].unique()
+        buyable_art_cdf = clipped_trans_cdf.query(f"art_id in {buyable_art_ids}")
 
-      bought_cust_ids = clipped_trans_cdf['customer_id'].unique()
-      bought_cust_cdf = clipped_trans_cdf.query(f"customer_id in {bought_cust_ids}")
+        bought_cust_ids = clipped_trans_cdf['customer_id'].unique()
+        bought_cust_cdf = clipped_trans_cdf.query(f"customer_id in {bought_cust_ids}")
 
 
-      base_cols = [
-          "customer_id",
-          "article_id",
-          "candidate_type",
-          "target_week",
-          "target",
-      ]
+        base_cols = [
+            "customer_id",
+            "article_id",
+            "candidate_type",
+            "target_week",
+            "target",
+        ]
+
+        y_cdf = make_target_df(clipped_trans_cdf)
+
+        #いろんなメソッドを使って候補生成
+        base_df = candidate_generation(
+            candidate_blocks, clipped_trans_cdf, buyable_art_cdf, bought_cust_cdf, y_cdf, target_customers = buyable_art_ids
+        )
+
+        base_df = negative_sampling(base_df, clipped_trans_cdf, y_cdf) #適当に間引くはず
 
 
-      candidate_df = pd.DataFrame()
+        art_feat_df, cust_feat_df, pair_feat_df = feature_generation(
+        feature_blocks, clipped_trans_cdf, buyable_art_cdf, bought_cust_cdf, target_customers = buyable_art_ids
+    )
 
-      #いろんなメソッドを使って候補生成
-      for candidate_block in blocks:
-          block_df = candidate_block(clipped_trans_cdf, buyable_art_cdf, bought_cust_cdf, target_customers = buyable_art_ids)
-          candidate_df = pd.concat([candidate_df, block_df])
+        # 特徴量作成
+        base_df = base_df.merge(cust_feat_df, how="left", on="customer_id")
+        base_df = base_df.merge(art_feat_df, how="left", on="article_id")
+        base_df = base_df.merge(pair_feat_df, how="left", on=["article_id", "customer_id"]
+        )
 
-      candidate_df = sampling(candidate_df, clipped_trans_cdf) #適当に間引くはず
+        if phase == "train":
+            # 学習データは複数予測週があるのでconcatする
+            train_df = pd.concat([train_df, base_df])
+        elif phase == "validate":
+            valid_df = base_df.copy()
 
-      # (Pending) 候補生成メソッドで取ってこれなかった正例を全部ブッコム
-      # positive_df = get_postive_pair(clipped_trans_cdf, columns=base_cols)
-      # positive_df["target"] = 1
+    train_df['query_id'] = train_df.groupby(['customer_id','target_week']).size().values
+    valid_df['query_id'] = valid_df.groupby(['customer_id','target_week']).size().values
 
-      # base_df = pd.concat([candidate_df, positive_df]).drop_duplicates(keep='first')
-    else:
-      # 推論候補集合取得
-      # クソデカになる & バッチで処理したいので後ほど作成でも良さそう。
-      # ここの実装の解像度がまだ低い
-      base_df = get_inference_data()
-
-  # 特徴量作成
-  base_df = base_df.merge(base_df, cust_feat_df, how="left", on="customer_id")
-  base_df = base_df.merge(base_df, art_feat_df, how="left", on="article_id")
-  base_df = base_df.merge(
-      base_df, pair_feat_df, how="left", on=["article_id", "customer_id"]
-  )
-
-  if phase == "train":
-      # 学習データは複数予測週があるのでconcatする
-      train_df = pd.concat([train_df, base_df])
-  elif phase == "validate":
-      valid_df = base_df.copy()
-  else:
-      test_df = base_df.copy()
-
+    return train_df, valid_df
 
 clf = lgb.LGBMRanker()
 clf.fit(train_df,valid_df)
 clf.predict(test_df)
+
+base_df = get_inference_data()
