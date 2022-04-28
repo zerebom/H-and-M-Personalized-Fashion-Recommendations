1572b6cc4d2bbde3a85e87a8ba19e5d97cbd8e1f
EC2 Default User <ec2-user@ip-172-31-12-66.ec2.internal>
2022-04-28 15:11:42 +0000

    add CustItemEmbBlock, ItemBiasIndicatorBlock, LifetimesBlock

--- higu/src/features.py
+++ higu/src/features.py
@@ -1,5 +1,10 @@
 import cudf
 import pandas as pd
+import lifetimes
+from lifetimes.utils import summary_data_from_transaction_data
+from cuml import TruncatedSVD
+from cuml.feature_extraction.text import TfidfVectorizer
+
 
 to_cdf = cudf.DataFrame.from_pandas
 
@@ -94,3 +99,73 @@ class ModeCategoryBlock(AbstractBaseBlock):
         )
         out_cdf[col_name] = out_cdf[col_name].astype("category")
         return out_cdf  # %%
+    
+class LifetimesBlock(AbstractBaseBlock):
+    def __init__(self, key_col, target_col):
+
+        self.key_col = key_col # 'customer_id'
+        self.target_col = target_col # 'price'
+
+    def transform(self, input_cdf: cudf.DataFrame):
+        price_mean_cust_dat= input_cdf.groupby([self.key_col, 't_dat'])[self.target_col].sum().reset_index().to_pandas()
+        out_df = summary_data_from_transaction_data(
+            price_mean_cust_dat, self.key_col, 't_dat', 
+            observation_period_end=price_mean_cust_dat.t_dat.max()# train:2020-09-08, valid: 2020-09-15 , test: 2020-09-22 
+        ).reset_index()
+        
+        out_cdf = cudf.from_pandas(out_df)
+
+        return out_cdf
+    
+    
+class CustItemEmbBlock(AbstractBaseBlock):
+    def __init__(self, key_col, enb_size, article_cdf):
+
+        self.key_col = key_col # 'customer_id'
+        self.enb_size = enb_size
+        self.article_cdf = article_cdf # parquetはtxtが空なのでローデータのcsvを使用
+        
+    def transform(self, input_cdf: cudf.DataFrame):
+        input_cdf = input_cdf.merge(self.article_cdf, on = 'article_id')
+        
+        # customerごとにtextを集約, これで顧客ごとに買う系統の服の情報が集まるイメージ
+        customer_desc_df = input_cdf.to_pandas().groupby(self.key_col)['text'].apply(list).apply(' '.join).reset_index()
+        customer_desc_cdf = cudf.from_pandas(customer_desc_df)
+        del customer_desc_df
+        
+        # TFIDF & TruncatedSVD
+        tfidf = TfidfVectorizer(min_df=3)
+        V_desc = tfidf.fit_transform(customer_desc_cdf["text"].fillna("nodesc"))
+        svd = TruncatedSVD(n_components=self.enb_size, random_state=0)
+        svd.fit(V_desc.todense())
+        V_desc = svd.transform(V_desc.todense())
+        
+        out_cdf = cudf.DataFrame(V_desc)
+        out_cdf.columns = [f"cust_itme_desc{col}" for col in out_cdf.columns] 
+        out_cdf['customer_id'] = customer_desc_cdf['customer_id']
+
+        return out_cdf
+    
+class ItemBiasIndicatorBlock(AbstractBaseBlock):
+    """
+    その商品を買う人に偏りがあるのかどうかといった指標
+    その商品はある一部の人から購入されているのか、万人に購入されているのか
+    例: 超奇抜なピンクの服 -> ある一部の人から購入されている
+    例: 万人に購入されている -> 黒い下着
+    """
+    def __init__(self, key_col, target_col):
+
+        self.key_col = key_col # 'article_id'
+        self.target_col = target_col # 'customer_id'
+        
+    def transform(self, input_cdf: cudf.DataFrame):
+        nunique_user_by_article = input_cdf.groupby(self.key_col)[self.target_col].nunique().reset_index()
+        nunique_user_by_article.columns =[self.key_col, "nunique_user_by_article"]
+
+        # すでに作成済み？？？
+        count_user_by_article = input_cdf.groupby(self.key_col)[self.target_col].count().reset_index()
+        count_user_by_article.columns =[self.key_col, "count_user_by_article"]
+
+        out_cdf = nunique_user_by_article.merge(count_user_by_article, on=self.key_col)
+        out_cdf["suitable_for_everyone"] = out_cdf["nunique_user_by_article"] / out_cdf["count_user_by_article"]
+        return out_cdf
